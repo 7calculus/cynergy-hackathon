1c6c3dda9a11747b97c5f1fa8327d3ec1dbbfd96
7calculus <garudsudhanwa@gmail.com>
2025-04-02 13:33:55 +0530

    Add files via upload
    coding relay

---
+++ noob_headquarters.py
@@ -0,0 +1,34 @@
+r1 = input('enter the first roman: ')
+r2 = input('enter the second roman: ')
+
+def convert(r):
+    num = 0
+    di = {'X':10, 'V':5, 'I': 1, 'C': 100, 'D': 500, 'M': 1000, 'L': 50}
+    for i in range(len(r)):
+        maxm = -1
+        maxm = max(maxm, di[r[i]])
+    for i in range(len(r)):
+        if(di[r[i]] < maxm):
+            num -= di[r[i]]
+        else:
+            num += di[r[i]]
+    return num
+
+def c_back(val):
+    di1 = {'X':10, 'V':5, 'I': 1, 'C': 100, 'D': 500, 'M': 1000, 'L': 50}
+    di2 = {10: 'X', 5: 'V', 1: 'I', 100: 'C', 500: 'D', 1000: 'M', 50: 'L'}
+    fnly = ''
+    if(val < 10):
+        for i in range(val // 10):
+            fnly += 'X'
+            #continue
+
+    return fnly
+
+
+
+print(c_back(convert(r1) + convert(r2)))    
+
+
+
+#Sudhanwa Gurucharan Garud
